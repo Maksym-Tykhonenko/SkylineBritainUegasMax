edf869b8a634e58e0521c59373bdb574ba54b6f2
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-11-11 00:28:19 +0200

    ccc

--- KingdoBritStackLine.js
+++ KingdoBritStackLine.js
@@ -335,7 +335,7 @@ const SlylineNavigator = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-11-08T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-11-13T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
